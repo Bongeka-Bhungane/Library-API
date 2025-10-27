43b2af18663079c23013bd7cf0d21e2146d2e62e
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-10-27 12:23:04 +0200

    did the update and delete functions for the authors

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -31,4 +31,35 @@ router.post("/", body("name").notEmpty().withMessage("Name id required"), (req: 
     res.status(201).json(newAuthor)
 })
 
+router.put("/:id", body("name").notEmpty().withMessage("name is requried!!"), (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const author = authors.find(a => a.id === id);
+
+    if(!author) return res.status(404).json({ message: "Author not found"})
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+
+        const { name, bio} = req.body;
+        author.name = name;
+        author.bio = bio;
+
+        res.json(author)
+   
+})
+
+router.delete("/:id", (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const authorIndex = authors.findIndex(a => a.id === id);
+
+    if (authorIndex === -1) {
+        return res.status(404).json({ message: "Author not found"})
+    } 
+
+    authors.splice(authorIndex, 1)
+    res.status(204).send()
+})
+
 export default router
