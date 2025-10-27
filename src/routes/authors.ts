0e778aa64c8d91f5dd3f30f1e3e1b0f0ae3bf2ea
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-10-27 09:47:20 +0200

    created a get funtion for the authors

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -1,7 +1,32 @@
 import { Router, Request, Response } from 'express';
-
+import { Author } from '../models/authors';
 import { body, param, validationResult } from 'express-validator';
 
 const router = Router()
 
-let authors: Author[] = []
+let authors: Author[] = [] 
+
+router.get("/", (req: Request, res:Response) => {
+    res.status(200).json(authors)
+})
+
+router.get("/:id", [param("id").isInt().withMessage("Id must be an integer")],(req: Request, res: Response) => {
+    const errors = validationResult(req)
+
+    console.log(errors, "errors from express-validator middleware")
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    const {id} = req.params
+    const author = authors.find((author) => author.id === parseInt(id))
+
+    if(!author){
+        return res.status(404).send("Author not found")
+    }
+
+    res.status(200).json(author)
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import { loggerMiddleware } from "./middleware/logger";
+import router from "./routes/authors"
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use(bodyParser.json())
 
 app.use(loggerMiddleware)
+app.use("/v1/authors", router)
 
 app.listen(PORT, () => {
     console.log(`srver is running on http://localhost:${PORT}`);
