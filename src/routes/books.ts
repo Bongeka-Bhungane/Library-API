74baf9731de5b5939794e5440a8e20c591befd40
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-10-27 15:32:01 +0200

    did the get and post functions on the books

---
+++ src/routes/books.ts
@@ -0,0 +1,58 @@
+import express, { Express } from "express"
+import { Router, Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator"
+import { Book, books } from "../models/books";
+import { Author, authors } from "../models/authors"
+import { error } from "console";
+
+const router = express.Router();
+let nextBookId = 1;
+
+const validateBook =[
+    body("title").notEmpty().withMessage("Title is required"),
+    body("year").isInt({ min: 1 }).withMessage("year must be a valid number"),
+    body("authorId").isInt({ min: 1}).withMessage("authorId must be positive integer")
+]
+
+router.post(
+  "/",
+  validateBook,
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(400).json({ message: "Invalid author id"})
+
+    const { title, year, authorId } = req.body;
+    const authorExists = authors.some(a => a.id === authorId)
+    if (!authorExists) return res.status(400).json({ message: "Book already exists for this author" })
+
+    const newBook: Book = { id: nextBookId++, title, year, authorId };
+    books.push(newBook);
+    res.status(201).json(newBook)
+  });
+
+  router.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.json(books)
+  })
+
+  router.get("/:id", validateBook,
+  (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt (req.params.id);
+    const book = books.find(b => b.id === id);
+    if (!book) return res.status(404).json({ message: "Book not found"})
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) return res.status(400).json({ erros: errors.array() });
+
+        const { title, year, authorId } = req.body;
+
+        const authorExists = authors.some(a => a.id === authorId)
+        if (!authorExists) return res.status(400).json({ message: "Invalid author Id"});
+
+        book.title = title;
+        book.year = year; 
+        book.authorId = authorId;
+
+        res.json(book)
+  })
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import { loggerMiddleware } from "./middleware/logger";
-import router from "./routes/authors"
+import authorRouter from "./routes/authors"
+import bookRouter from "./routes/books"
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -10,7 +11,12 @@ app.use(express.json())
 app.use(bodyParser.json())
 
 app.use(loggerMiddleware)
-app.use("/authors", router)
+app.use("/authors", authorRouter)
+app.use("/books", bookRouter)
+
+app.get("/", (req, res) => {
+    res.json({ message: "Library API is running"})
+})
 
 app.listen(PORT, () => {
     console.log(`srver is running on http://localhost:${PORT}`);
